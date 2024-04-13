c4da1a4e4c923325b40b532088c400bb34524f6e
rocha101 <eduardo@inovarsistemas.com>
2024-04-12 21:45:04 -0300

    update

--- prisma/migrations/20240411200743_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `weekDay` on the `Train` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Train" DROP COLUMN "weekDay",
-ADD COLUMN     "weekDays" "WeekDay"[];
