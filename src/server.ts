6f6f9a7eb23419b6e39f882b41b2f869b399501b
rocha101 <eduardo@inovarsistemas.com>
2024-04-12 22:23:12 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ import qs from "qs";
 export const prisma = new PrismaClient();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 var cors = require("cors");
 
 async function main() {
